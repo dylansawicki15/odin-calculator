51808ee4a56175912ce3da4a0861d3938b3a5ef4
dylansawicki15 <dylansawicki15@gmail.com>
2026-08-27 18:43:37 +0200

    added four simple operators

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+function operate(firstNum, secondNum, operator) {}
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
